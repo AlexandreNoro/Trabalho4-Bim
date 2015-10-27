dba7e63c8f85758eca501aec0944ae6d076a25db
Alexandre H. Noro <xande10_noro@hotmail.com>
2015-10-27 21:18:59 -0200

    Feito as Telas, tendo como base a aula anterior e copiado do professor sobre o block

---
+++ src/main/java/Telas/TelaCadastroCliente.java
@@ -0,0 +1,18 @@
+package Telas;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class TelaCadastroCliente extends MolduraAbstrata {
+
+	public TelaCadastroCliente() {
+		super();
+	}
+
+	@Override
+	protected void configuraMiolo() {
+		super.add(new MioloCadastroCliente(), BorderLayout.CENTER);
+	}
+
+}
