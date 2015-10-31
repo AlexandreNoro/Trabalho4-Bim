b184170cad7b2ed14ecc0a08c5edc7fbc3898b12
Alexandre H. Noro <xande10_noro@hotmail.com>
2015-10-31 21:03:38 -0200

    Criado Construtures e implementado alguns métodos no ClienteDaoAcesso

--- src/main/java/AcessoAoBanco/ClienteDaoAcesso.java
+++ src/main/java/AcessoAoBanco/ClienteDaoAcesso.java
@@ -1,38 +1,158 @@
 package AcessoAoBanco;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import br.univel.cadastroCliente.Cliente;
+import br.univel.cadastroCliente.Estado;
+import br.univel.cadastroCliente.Genero;
 
 public class ClienteDaoAcesso implements ClienteDao {
 
+	private static Connection con;
+
+	private static ClienteDaoAcesso instanciar;
+	
+	//Construtor da classe
+	private ClienteDaoAcesso() {
+
+	}
+	//Método para retornar nova Instância
+	public static ClienteDaoAcesso getNovaInstancia() {
+		if (instanciar == null)
+			return instanciar = new ClienteDaoAcesso();
+		return instanciar;
+	}
+	
+	//Método para abrir conexao com o banco
+	private void abrirConexao() throws SQLException {
+
+		String url = "jdbc:h2:~/Alexandre";
+		String user = "sa";
+		String password = "sa";
+		con = DriverManager.getConnection(url, user, password);
+
+	}
+
+	//Método para fechar conexão com o banco
+	private void fecharConexao() throws SQLException {
+
+		con.close();
+
+	}
+
+	//Método inserir
 	@Override
 	public void inserir(Cliente c) {
-		// TODO Auto-generated method stub
-		
+
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(
+					"INSERT INTO CLIENTE(ID,NOME,ENDERECO,TELEFONE,CIDADE,ESTADO,GENERO,EMAIL) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+			ps.setInt(1, c.getId());
+			ps.setString(2, c.getNome());
+			ps.setString(3, c.getEndereco());
+			ps.setString(4, c.getTelefone());
+			ps.setString(5, c.getCidade());
+			ps.setObject(6, c.getEstado().getNome());
+			ps.setObject(7, c.getGenero().getNome());
+			ps.setString(8, c.getEmail());
+
+			int res = ps.executeUpdate();
+
+			ps.close();
+
+			System.out.println(res + "Inserido com sucesso");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
+	//Método atualizar
 	@Override
 	public void atualizar(Cliente c) {
-		// TODO Auto-generated method stub
-		
+
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(
+					"UPDATE CLIENTE SET NOME = ?,ENDERECO = ?,TELEFONE = ?,ESTADO = ?,GENERO = ?, EMAIL = ?  WHERE ID ="
+							+ c.getId());
+
+			ps.setInt(1, c.getId());
+			ps.setString(2, c.getNome() + "");
+			ps.setString(3, c.getEndereco() + "");
+			ps.setString(4, c.getTelefone() + "");
+			ps.setString(5, c.getCidade() + "");
+			ps.setObject(6, c.getEstado() + "");
+			ps.setObject(7, c.getGenero() + "");
+			ps.setString(8, c.getEmail() + "");
+
+			int res = ps.executeUpdate();
+
+			ps.close();
+
+			System.out.println(res + "Alterado com Sucesso");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
+	//Método excluir 
 	@Override
 	public void excluir(Cliente c) {
-		// TODO Auto-generated method stub
-		
+
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement("DELETE FROM CLIENTE WHERE ID =" + c.getId());
+
+			int res = ps.executeUpdate();
+
+			ps.close();
+
+			System.out.println(res + "Excluído com Sucesso");
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
 	}
 
+	//Método para buscar
 	@Override
 	public Cliente buscar(int id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Statement st = null;
+		ResultSet rs = null;
+		Cliente c = null;
+		Estado uf = Estado.PR;
+		Genero gn = Genero.M;
+		try {
+			st = con.createStatement();
+			rs = st.executeQuery(
+					"SELECT NOME,ENDERECO,TELEFONE,CIDADE,ESTADO,GENERO,EMAIL  FROM CLIENTE WHERE ID=" + id);
+			rs.next();
+			c = new Cliente(id, rs.getString("NOME"), rs.getString("ENDERECO"), rs.getString("TELEFONE"),
+					rs.getString("CIDADE"), uf.validar(rs.getObject("ESTADO")), gn.validar(rs.getObject("GENERO")),
+					rs.getString("EMAIL"));
+		} catch (SQLException e) {
+			System.out.println("Ocorreu um erro ao buscar o Cliente desejado!\n" + e.getMessage());
+		}
+		return c;
 	}
 
+	//Método para listar 
 	@Override
 	public List<Cliente> listar() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
--- src/main/java/br/univel/cadastroCliente/Cliente.java
+++ src/main/java/br/univel/cadastroCliente/Cliente.java
@@ -1,5 +1,6 @@
 package br.univel.cadastroCliente;
 
+
 public class Cliente {
 
 	private int id;
@@ -8,6 +9,7 @@ public class Cliente {
 	private String endereco;
 	private String cidade;
 	private Estado estado;
+	private Genero genero;
 	private String email;
 
 	public int getId() {
@@ -74,6 +76,28 @@ public class Cliente {
 		this.genero = genero;
 	}
 
-	private Genero genero;
+	public Cliente(int id, String nome, String endereco, String telefone, String cidade, Estado uf, Genero gn,
+			String email) {
+
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.cidade = cidade;
+		this.estado = uf;
+		this.genero = gn;
+		this.email = email;
+	}
+
+	public Cliente(int id) {
+		super();
+		this.id = id;
+	}
+
+	public String toString() {
+		return "Cliente[ " + this.nome + ", " + this.endereco + ", " + this.telefone + ", " + this.cidade + ", "
+				+ this.estado.getNome() + ", " + this.genero.getNome() + ", " + this.email + "] \n";
+	}
 
 }
