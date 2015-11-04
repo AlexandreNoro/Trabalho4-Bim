77974dbe3c3b65810d0ab80b66aae31b3f0666df
Alexandre H. Noro <xande10_noro@hotmail.com>
2015-11-04 09:48:57 -0200

    Criado tela de relatório de Produto

--- src/main/java/Relatorios/RelatorioProduto.java
+++ src/main/java/Relatorios/RelatorioProduto.java
@@ -4,60 +4,63 @@ import javax.swing.JPanel;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
 import java.awt.Insets;
 import javax.swing.JComboBox;
-import javax.swing.JButton;
 import java.awt.Font;
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-public class RelatorioCliente extends JPanel {
-	private JTable tablecliente;
+public class RelatorioProduto extends JPanel {
+	private JTextField textField;
+	private JTable tableproduto;
 
 	/**
 	 * Create the panel.
 	 */
-	public RelatorioCliente() {
+	public RelatorioProduto() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 95, 0, 129, 0};
+		gridBagLayout.columnWidths = new int[]{0, 120, 80, 110, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
-		JLabel lblFiltrarPorEstado = new JLabel("Filtrar por Estado:");
-		lblFiltrarPorEstado.setFont(new Font("Arial Narrow", Font.BOLD, 11));
-		GridBagConstraints gbc_lblFiltrarPorEstado = new GridBagConstraints();
-		gbc_lblFiltrarPorEstado.anchor = GridBagConstraints.EAST;
-		gbc_lblFiltrarPorEstado.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFiltrarPorEstado.gridx = 0;
-		gbc_lblFiltrarPorEstado.gridy = 0;
-		add(lblFiltrarPorEstado, gbc_lblFiltrarPorEstado);
+		JLabel lblMargemDeLucro = new JLabel("Margem de Lucro:");
+		lblMargemDeLucro.setFont(new Font("Arial Narrow", Font.BOLD, 11));
+		GridBagConstraints gbc_lblMargemDeLucro = new GridBagConstraints();
+		gbc_lblMargemDeLucro.anchor = GridBagConstraints.EAST;
+		gbc_lblMargemDeLucro.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMargemDeLucro.gridx = 0;
+		gbc_lblMargemDeLucro.gridy = 0;
+		add(lblMargemDeLucro, gbc_lblMargemDeLucro);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblCategoria = new JLabel("Categoria:");
+		lblCategoria.setFont(new Font("Arial Narrow", Font.BOLD, 11));
+		GridBagConstraints gbc_lblCategoria = new GridBagConstraints();
+		gbc_lblCategoria.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCategoria.anchor = GridBagConstraints.EAST;
+		gbc_lblCategoria.gridx = 2;
+		gbc_lblCategoria.gridy = 0;
+		add(lblCategoria, gbc_lblCategoria);
 		
 		JComboBox comboBox = new JComboBox();
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridx = 3;
 		gbc_comboBox.gridy = 0;
 		add(comboBox, gbc_comboBox);
-		
-		JLabel lblFiltrarPorCidade = new JLabel("Filtrar por Cidade:");
-		lblFiltrarPorCidade.setFont(new Font("Arial Narrow", Font.BOLD, 11));
-		GridBagConstraints gbc_lblFiltrarPorCidade = new GridBagConstraints();
-		gbc_lblFiltrarPorCidade.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFiltrarPorCidade.anchor = GridBagConstraints.EAST;
-		gbc_lblFiltrarPorCidade.gridx = 2;
-		gbc_lblFiltrarPorCidade.gridy = 0;
-		add(lblFiltrarPorCidade, gbc_lblFiltrarPorCidade);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox_1.gridx = 3;
-		gbc_comboBox_1.gridy = 0;
-		add(comboBox_1, gbc_comboBox_1);
 		
 		JButton btnFiltrar = new JButton("Filtrar");
 		btnFiltrar.setFont(new Font("Arial Narrow", Font.BOLD, 11));
@@ -80,8 +83,8 @@ public class RelatorioCliente extends JPanel {
 		JButton btnGerarPdf = new JButton("Gerar PDF");
 		btnGerarPdf.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_btnGerarPdf = new GridBagConstraints();
-		gbc_btnGerarPdf.insets = new Insets(0, 0, 5, 0);
 		gbc_btnGerarPdf.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnGerarPdf.insets = new Insets(0, 0, 5, 0);
 		gbc_btnGerarPdf.gridx = 3;
 		gbc_btnGerarPdf.gridy = 1;
 		add(btnGerarPdf, gbc_btnGerarPdf);
@@ -95,9 +98,9 @@ public class RelatorioCliente extends JPanel {
 		gbc_scrollPane.gridy = 2;
 		add(scrollPane, gbc_scrollPane);
 		
-		tablecliente = new JTable();
-		tablecliente.setFont(new Font("Consolas", Font.BOLD, 11));
-		scrollPane.setViewportView(tablecliente);
+		tableproduto = new JTable();
+		tableproduto.setFont(new Font("Consolas", Font.BOLD, 11));
+		scrollPane.setViewportView(tableproduto);
 
 	}
 
