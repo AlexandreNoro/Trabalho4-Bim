779680a01ffa480beca478fda81587bf6e9b62c5
Alexandre H. Noro <xande10_noro@hotmail.com>
2015-11-04 10:20:27 -0200

    Atualizado tela de cadastro de usuario

--- src/main/java/Telas/MioloCadastroUsuario.java
+++ src/main/java/Telas/MioloCadastroUsuario.java
@@ -6,25 +6,35 @@ import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class MioloCadastroUsuario extends JPanel {
 	private JTextField txfId;
 	private JTextField txfIdcliente;
 	private JTextField txf_cliente;
 	private JTextField txf_senha;
+	private JButton btnGravar;
+	private JButton btnEditar;
+	private JButton btnDeletar;
+	private JScrollPane scrollPane;
+	private JTable tablemiolousuario;
 
 	/**
 	 * Create the panel.
 	 */
 	public MioloCadastroUsuario() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{0, 150, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JLabel lblId = new JLabel("Id");
+		lblId.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_lblId = new GridBagConstraints();
 		gbc_lblId.anchor = GridBagConstraints.EAST;
 		gbc_lblId.insets = new Insets(0, 0, 5, 5);
@@ -33,8 +43,9 @@ public class MioloCadastroUsuario extends JPanel {
 		add(lblId, gbc_lblId);
 		
 		txfId = new JTextField();
+		txfId.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_txfId = new GridBagConstraints();
-		gbc_txfId.insets = new Insets(0, 0, 5, 0);
+		gbc_txfId.insets = new Insets(0, 0, 5, 5);
 		gbc_txfId.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txfId.gridx = 1;
 		gbc_txfId.gridy = 0;
@@ -42,6 +53,7 @@ public class MioloCadastroUsuario extends JPanel {
 		txfId.setColumns(10);
 		
 		JLabel lblIdCliente = new JLabel("Id Cliente");
+		lblIdCliente.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_lblIdCliente = new GridBagConstraints();
 		gbc_lblIdCliente.anchor = GridBagConstraints.EAST;
 		gbc_lblIdCliente.insets = new Insets(0, 0, 5, 5);
@@ -50,8 +62,9 @@ public class MioloCadastroUsuario extends JPanel {
 		add(lblIdCliente, gbc_lblIdCliente);
 		
 		txfIdcliente = new JTextField();
+		txfIdcliente.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_txfIdcliente = new GridBagConstraints();
-		gbc_txfIdcliente.insets = new Insets(0, 0, 5, 0);
+		gbc_txfIdcliente.insets = new Insets(0, 0, 5, 5);
 		gbc_txfIdcliente.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txfIdcliente.gridx = 1;
 		gbc_txfIdcliente.gridy = 1;
@@ -59,6 +72,7 @@ public class MioloCadastroUsuario extends JPanel {
 		txfIdcliente.setColumns(10);
 		
 		JLabel lbl_cliente = new JLabel("Cliente");
+		lbl_cliente.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_lbl_cliente = new GridBagConstraints();
 		gbc_lbl_cliente.anchor = GridBagConstraints.EAST;
 		gbc_lbl_cliente.insets = new Insets(0, 0, 5, 5);
@@ -67,8 +81,9 @@ public class MioloCadastroUsuario extends JPanel {
 		add(lbl_cliente, gbc_lbl_cliente);
 		
 		txf_cliente = new JTextField();
+		txf_cliente.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_txf_cliente = new GridBagConstraints();
-		gbc_txf_cliente.insets = new Insets(0, 0, 5, 0);
+		gbc_txf_cliente.insets = new Insets(0, 0, 5, 5);
 		gbc_txf_cliente.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txf_cliente.gridx = 1;
 		gbc_txf_cliente.gridy = 2;
@@ -76,20 +91,62 @@ public class MioloCadastroUsuario extends JPanel {
 		txf_cliente.setColumns(10);
 		
 		JLabel lbl_senha = new JLabel("Senha");
+		lbl_senha.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_lbl_senha = new GridBagConstraints();
 		gbc_lbl_senha.anchor = GridBagConstraints.EAST;
-		gbc_lbl_senha.insets = new Insets(0, 0, 0, 5);
+		gbc_lbl_senha.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_senha.gridx = 0;
 		gbc_lbl_senha.gridy = 3;
 		add(lbl_senha, gbc_lbl_senha);
 		
 		txf_senha = new JTextField();
+		txf_senha.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_txf_senha = new GridBagConstraints();
+		gbc_txf_senha.insets = new Insets(0, 0, 5, 5);
 		gbc_txf_senha.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txf_senha.gridx = 1;
 		gbc_txf_senha.gridy = 3;
 		add(txf_senha, gbc_txf_senha);
 		txf_senha.setColumns(10);
+		
+		btnGravar = new JButton("Gravar");
+		btnGravar.setFont(new Font("Arial Narrow", Font.BOLD, 11));
+		GridBagConstraints gbc_btnGravar = new GridBagConstraints();
+		gbc_btnGravar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnGravar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnGravar.gridx = 2;
+		gbc_btnGravar.gridy = 4;
+		add(btnGravar, gbc_btnGravar);
+		
+		btnEditar = new JButton("Editar");
+		btnEditar.setFont(new Font("Arial Narrow", Font.BOLD, 11));
+		GridBagConstraints gbc_btnEditar = new GridBagConstraints();
+		gbc_btnEditar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEditar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnEditar.gridx = 3;
+		gbc_btnEditar.gridy = 4;
+		add(btnEditar, gbc_btnEditar);
+		
+		btnDeletar = new JButton("Excluir");
+		btnDeletar.setFont(new Font("Arial Narrow", Font.BOLD, 11));
+		GridBagConstraints gbc_btnDeletar = new GridBagConstraints();
+		gbc_btnDeletar.insets = new Insets(0, 0, 5, 0);
+		gbc_btnDeletar.gridx = 4;
+		gbc_btnDeletar.gridy = 4;
+		add(btnDeletar, gbc_btnDeletar);
+		
+		scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 5;
+		gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 5;
+		add(scrollPane, gbc_scrollPane);
+		
+		tablemiolousuario = new JTable();
+		tablemiolousuario.setFont(new Font("Arial Narrow", Font.BOLD, 11));
+		scrollPane.setViewportView(tablemiolousuario);
 
 	}
 
