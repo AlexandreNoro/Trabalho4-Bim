fafee7eaba09b54fae18e17524635670b81d034e
Alexandre H. Noro <xande10_noro@hotmail.com>
2015-11-04 11:14:20 -0200

    Feito tabelaUsuario

--- src/main/java/Tables/TabelaUsuario.java
+++ src/main/java/Tables/TabelaUsuario.java
@@ -1,5 +1,85 @@
 package Tables;
 
-public class TabelaUsuario {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+import AcessoAoBanco.UsuarioDaoAcesso;
+import br.univel.cadastroCliente.Usuario;
+
+public class TabelaUsuario extends AbstractTableModel {
+
+	List<Usuario> lista = new ArrayList<>();
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return lista.size();
+	}
+
+	@Override
+	public Object getValueAt(int linha, int coluna) {
+
+		Usuario user = lista.get(linha);
+		switch (coluna) {
+		case 0:
+			return user.getId();
+		case 1:
+			return user.getIdCliente();
+		case 2:
+			return user.getCliente();
+		case 3:
+			return user.getSenha();
+
+		default:
+			return "Nao Existe";
+
+		}
+
+	}
+
+	public String getColumnName(int coluna) {
+
+		switch (coluna) {
+
+		case 0:
+			return "ID USUÁRIO";
+		case 1:
+			return "ID CLIENTE";
+		case 2:
+			return "USUÁRIO";
+		case 3:
+			return "SENHA";
+
+		default:
+			return "Nao Existe";
+		}
+	}
+
+	public List<Usuario> listar() {
+		UsuarioDaoAcesso user = new UsuarioDaoAcesso();
+		return lista = user.listar();
+	}
+
+	public void addLista(List<Usuario> user) {
+		this.lista = user;
+		this.fireTableStructureChanged();
+	}
+
+	public void atualizaLista(int in, Usuario user) {
+		this.lista.set(in, user);
+		this.fireTableStructureChanged();
+	}
+
+	public void excluir(int in) {
+		this.lista.remove(in);
+		this.fireTableStructureChanged();
+	}
 
 }
