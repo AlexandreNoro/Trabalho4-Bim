0778d5fb57c1c91d25fd7b7c1335220f5a87b0df
Alexandre H. Noro <xande10_noro@hotmail.com>
2015-11-06 19:19:45 -0200

    Feito alguns métodos na classe relatórioCliente

--- src/main/java/Relatorios/RelatorioCliente.java
+++ src/main/java/Relatorios/RelatorioCliente.java
@@ -3,28 +3,49 @@ package Relatorios;
 import javax.swing.JPanel;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import Tables.TabelaCliente;
+import br.univel.cadastroCliente.Cliente;
+import br.univel.cadastroCliente.Estado;
+import br.univel.cadastroCliente.Genero;
+
 public class RelatorioCliente extends JPanel {
 	private JTable tablecliente;
+	private JComboBox<String> cmbx_estado;
+	private JComboBox<String> cmbx_cidade;
+
+	private static String ARQ_PDF;
+	private String arq = "/////";
+	private List<Cliente> listacliente;
+	private TabelaCliente tabelaCliente;
+	private String comando = "SELECT IDCOD_C, NOME, TELEONE, ENDERECO, GENERO, EMAIL FROM CLIENTE";
 
 	/**
 	 * Create the panel.
 	 */
 	public RelatorioCliente() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 95, 0, 129, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 0, 95, 0, 129, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
-		
+
 		JLabel lblFiltrarPorEstado = new JLabel("Filtrar por Estado:");
 		lblFiltrarPorEstado.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_lblFiltrarPorEstado = new GridBagConstraints();
@@ -33,15 +54,22 @@ public class RelatorioCliente extends JPanel {
 		gbc_lblFiltrarPorEstado.gridx = 0;
 		gbc_lblFiltrarPorEstado.gridy = 0;
 		add(lblFiltrarPorEstado, gbc_lblFiltrarPorEstado);
-		
-		JComboBox comboBox = new JComboBox();
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-		gbc_comboBox.gridx = 1;
-		gbc_comboBox.gridy = 0;
-		add(comboBox, gbc_comboBox);
-		
+
+		cmbx_estado = new JComboBox();
+		cmbx_estado.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent me) {
+				if (me.getClickCount() == 2) {
+					filtraEstado();
+				}
+			}
+		});
+		GridBagConstraints gbc_cmbx_estado = new GridBagConstraints();
+		gbc_cmbx_estado.fill = GridBagConstraints.HORIZONTAL;
+		gbc_cmbx_estado.insets = new Insets(0, 0, 5, 5);
+		gbc_cmbx_estado.gridx = 1;
+		gbc_cmbx_estado.gridy = 0;
+		add(cmbx_estado, gbc_cmbx_estado);
+
 		JLabel lblFiltrarPorCidade = new JLabel("Filtrar por Cidade:");
 		lblFiltrarPorCidade.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_lblFiltrarPorCidade = new GridBagConstraints();
@@ -50,16 +78,31 @@ public class RelatorioCliente extends JPanel {
 		gbc_lblFiltrarPorCidade.gridx = 2;
 		gbc_lblFiltrarPorCidade.gridy = 0;
 		add(lblFiltrarPorCidade, gbc_lblFiltrarPorCidade);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox_1.gridx = 3;
-		gbc_comboBox_1.gridy = 0;
-		add(comboBox_1, gbc_comboBox_1);
-		
+
+		cmbx_cidade = new JComboBox();
+		cmbx_cidade.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent me) {
+				if (me.getClickCount() == 2) {
+					filtraCidade();
+				}
+			}
+		});
+		GridBagConstraints gbc_cmbx_cidade = new GridBagConstraints();
+		gbc_cmbx_cidade.fill = GridBagConstraints.HORIZONTAL;
+		gbc_cmbx_cidade.insets = new Insets(0, 0, 5, 0);
+		gbc_cmbx_cidade.gridx = 3;
+		gbc_cmbx_cidade.gridy = 0;
+		add(cmbx_cidade, gbc_cmbx_cidade);
+
 		JButton btnFiltrar = new JButton("Filtrar");
+		btnFiltrar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				filtrarEstCid();
+
+			}
+		});
 		btnFiltrar.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_btnFiltrar = new GridBagConstraints();
 		gbc_btnFiltrar.fill = GridBagConstraints.HORIZONTAL;
@@ -67,8 +110,16 @@ public class RelatorioCliente extends JPanel {
 		gbc_btnFiltrar.gridx = 1;
 		gbc_btnFiltrar.gridy = 1;
 		add(btnFiltrar, gbc_btnFiltrar);
-		
+
 		JButton btnAtualizar = new JButton("Atualizar");
+		btnAtualizar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				carregartabela();
+
+			}
+		});
 		btnAtualizar.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_btnAtualizar = new GridBagConstraints();
 		gbc_btnAtualizar.fill = GridBagConstraints.HORIZONTAL;
@@ -76,7 +127,7 @@ public class RelatorioCliente extends JPanel {
 		gbc_btnAtualizar.gridx = 2;
 		gbc_btnAtualizar.gridy = 1;
 		add(btnAtualizar, gbc_btnAtualizar);
-		
+
 		JButton btnGerarPdf = new JButton("Gerar PDF");
 		btnGerarPdf.setFont(new Font("Arial Narrow", Font.BOLD, 11));
 		GridBagConstraints gbc_btnGerarPdf = new GridBagConstraints();
@@ -85,7 +136,7 @@ public class RelatorioCliente extends JPanel {
 		gbc_btnGerarPdf.gridx = 3;
 		gbc_btnGerarPdf.gridy = 1;
 		add(btnGerarPdf, gbc_btnGerarPdf);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.gridwidth = 4;
@@ -94,10 +145,88 @@ public class RelatorioCliente extends JPanel {
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 2;
 		add(scrollPane, gbc_scrollPane);
-		
+
 		tablecliente = new JTable();
 		tablecliente.setFont(new Font("Consolas", Font.BOLD, 11));
 		scrollPane.setViewportView(tablecliente);
+
+		carregartabela();
+
+		mostradados();
+
+	}
+
+	private void carregartabela() {
+		tabelaCliente = new TabelaCliente();
+		listacliente = tabelaCliente.listar();
+		new Thread(new Runnable() {
+			public void run() {
+				tablecliente.setModel(tabelaCliente);
+			}
+		}).start();
+
+	}
+
+	private void mostradados() {
+		int in = 0;
+
+		for (Estado e : Estado.values()) {
+			if (in == 0) {
+				cmbx_estado.addItem("");
+				in = 1;
+			}
+			cmbx_estado.addItem(e.getNome());
+		}
+
+		for (int x = 0; x < listacliente.size(); x++) {
+			in = 0;
+			if (x == 0) {
+				cmbx_cidade.addItem("");
+			}
+
+			for (int y = 0; y < cmbx_cidade.getItemCount(); y++) {
+				if (listacliente.get(x).getCidade().equals(cmbx_cidade.getItemAt(y).toString()))
+					in++;
+				if (in > 1)
+					break;
+			}
+			if (in < 1)
+				cmbx_cidade.addItem(listacliente.get(x).getCidade());
+		}
+	}
+
+	protected void filtrarEstCid() {
+		if (cmbx_cidade.getSelectedItem() != "" && cmbx_estado.getSelectedItem() != "") {
+			StringBuilder filtracomando = new StringBuilder();
+			filtracomando.append(comando + "WHERE ESTADO = '" + Estado.validar(cmbx_estado.getSelectedItem().toString())
+					+ "'AND CIDADE = '" + cmbx_cidade.getSelectedItem().toString() + "'");
+
+			listacliente = tabelaCliente.mostraRelatorio(filtracomando.toString());
+			tablecliente.setModel(tabelaCliente);
+		} else {
+			JOptionPane.showMessageDialog(null, "Escolha a Cidade e o Estado que deseja Filtrar");
+		}
+	}
+
+	protected void filtraCidade() {
+		if (cmbx_cidade.getSelectedItem() != "") {
+			StringBuilder filtracomando = new StringBuilder();
+			filtracomando.append(comando + "WHERE CIDADE = '" + cmbx_cidade.getSelectedItem() + "'");
+			listacliente = tabelaCliente.mostraRelatorio(filtracomando.toString());
+			tablecliente.setModel(tabelaCliente);
+		}
+
+	}
+
+	protected void filtraEstado() {
+
+		if (cmbx_estado.getSelectedItem() != "") {
+			StringBuilder filtracomando = new StringBuilder();
+			filtracomando.append(
+					comando + " WHERE ESTADO = '" + Estado.validar(cmbx_estado.getSelectedItem().toString()) + "'");
+			listacliente = tabelaCliente.mostraRelatorio(filtracomando.toString());
+			tablecliente.setModel(tabelaCliente);
+		}
 
 	}
 
