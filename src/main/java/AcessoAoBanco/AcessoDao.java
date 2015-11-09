6e8778daed0f04456a339e6282e71b6b9c3c693a
Alexandre H. Noro <xande10_noro@hotmail.com>
2015-11-09 11:11:29 -0200

    Corrigido erros

--- src/main/java/AcessoAoBanco/AcessoDao.java
+++ src/main/java/AcessoAoBanco/AcessoDao.java
@@ -2,18 +2,16 @@ package AcessoAoBanco;
 
 import java.util.List;
 
-import br.univel.cadastroCliente.Cliente;
-
 //Interface ClienteDao: Métodos CRUD para acesso ao banco
-public interface AcessoDao<A> {
+public interface AcessoDao<T> {
 
-	public void inserir(A acesso);
+	public void inserir(T acesso);
 
-	public void atualizar(A acesso);
+	public void atualizar(T acesso);
 
 	public void excluir(int acesso);
 
-	public A buscar(int acesso);
+	public T buscar(int acesso);
 
-	public List<A> listar();
+	public List<T> listar();
 }
