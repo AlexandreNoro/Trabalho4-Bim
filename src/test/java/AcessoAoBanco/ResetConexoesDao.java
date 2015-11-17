a0d2f8d8c33534f119ce3f92e45b020a3fef7863
Alexandre H. Noro <xande10_noro@hotmail.com>
2015-11-17 19:15:47 -0200

    Classe que reseta conexoes do banco. e comecado Junit do ClienteDao

---
+++ src/test/java/AcessoAoBanco/ResetConexoesDao.java
@@ -0,0 +1,36 @@
+package AcessoAoBanco;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ResetConexoesDao {
+
+	Connection conexao = ConectarBanco.getInstace().abreConexao();
+
+	public void reset(String nomeTable) {
+		try {
+			PreparedStatement ps;
+			if ("USUARIO".equals(nomeTable)) {
+				ps = getConexao().prepareStatement("DELETE FROM " + nomeTable + " WHERE ID_U > 1");
+				ps.executeUpdate();
+				ps = getConexao().prepareStatement("ALTER TABLE " + nomeTable + " AUTO_INCREMENT = 2");
+				ps.executeUpdate();
+				ps.close();
+			} else {
+				ps = getConexao().prepareStatement("DELETE FROM " + nomeTable);
+				ps.executeUpdate();
+				ps = getConexao().prepareStatement("ALTER TABLE " + nomeTable + " AUTO_INCREMENT = 1");
+				ps.executeUpdate();
+				ps.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConexao() {
+		return conexao;
+	}
+
+}
---
+++ src/test/java/AcessoAoBanco/TesteClienteDao.java
@@ -0,0 +1,39 @@
+package AcessoAoBanco;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteClienteDao {
+
+	@Test
+	public void testInserir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAtualizar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExcluir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConexao() {
+		fail("Not yet implemented");
+	}
+
+}
